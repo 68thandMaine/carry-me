ab8c6dfe0514202d4a384efe9769ae8e862d483f
Chris Rudnicky <chrisrudnicky@gmail.com>
2019-10-03 17:54:00 -0700

    update circleci file

--- server/_tests_/entity-routes.test.js
+++ server/_tests_/entity-routes.test.js
@@ -9,50 +9,43 @@ const mockEntity = require('./mock-data/mock-entity');
 const request = supertest(app);
 
 describe('Entity Endpoints', () => {
-  beforeAll(async (done) => {
+  beforeAll(async () => {
     const url = 'mongodb://localhost/test';
     await mongoose.connect(url, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    done();
   });
-  afterEach(async (done) => {
+  afterEach(async () => {
     await Entity.deleteMany();
-    done();
   });
 
-  it('POST / Should save an Entity to the database', async (done) => {
+  it('POST / Should save an Entity to the database', async () => {
     const res = await request.post('/carryme')
       .send(mockEntity[0])
       .set('Accept', 'application/json');
     expect(res.status).toBe(200);
-    done();
     const entity = await Entity.findOne({ email: mockEntity[0].email });
     expect(entity.entityName).toBeTruthy();
     expect(entity.email).toBeTruthy();
     expect(entity.password).toBeTruthy();
-    done();
   });
-  it('POST / Should return validation error message if missing required properties', async (done) => {
+  it('POST / Should return validation error message if missing required properties', async () => {
     const res = await request.post('/carryme')
       .send(mockEntity[1]);
     expect(res.text).toBe('Entity validation failed');
-    done();
   });
-  it('GET / Should return empty with 200 status code if no Entities are in the DB', async (done) => {
+  it('GET / Should return empty with 200 status code if no Entities are in the DB', async () => {
     const res = await request.get('/carryme');
     expect(res.body.length).toBe(0);
     expect(res.status).toBe(200);
-    done();
   });
-  it('GET / Should return all entites in the DB', async (done) => {
+  it('GET / Should return all entites in the DB', async () => {
     await Entity.insertMany(mockEntity[0]);
     const res = await request.get('/carryme');
     expect(res.body.length).toBe(1);
-    done();
   });
-  it('GET /:id should get one Entity', async (done) => {
+  it('GET /:id should get one Entity', async () => {
     // Insert the one entry into the mock db.
     // Return the entry with the proven get method
     // Collect the ID from the return.
@@ -64,9 +57,8 @@ describe('Entity Endpoints', () => {
     const res = await request.get(`/carryme/${entityID}`);    
     expect(res.statusCode).toBe(200);
     expect(res.body._id).toEqual(entityID);
-    done();
   });
-  it('DELETE /:id deletes an entity from the DB', async (done) => {
+  it('DELETE /:id deletes an entity from the DB', async () => {
     await Entity.insertMany(mockEntity[0]);
     const entities = await request.get('/carryme');
     const entityID = entities.body[0]._id;
@@ -77,9 +69,8 @@ describe('Entity Endpoints', () => {
     expect(deleteUser.text).toEqual('Deleted successfully');
     const notFound = await request.get(`/carryme/${entityID}`);
     expect(notFound.text).toEqual('Resource not found');
-    done();
   });
-  it('PUT /:id should update an entity', async (done) => {
+  it('PUT /:id should update an entity', async () => {
     await Entity.insertMany(mockEntity[0]);
     const entities = await request.get('/carryme');
     const entityID = entities.body[0]._id;
@@ -91,9 +82,8 @@ describe('Entity Endpoints', () => {
     expect(updatedEntity.statusCode).toBe(200);
     expect(updatedEntity.body.entityName).toBe('Dope Grills');
     expect(updatedEntity.body).not.toEqual({});
-    done();
   });
-  it('PUT /:id should return "Validation failed" if invalid entry', async (done) => {
+  it('PUT /:id should return "Validation failed" if invalid entry', async () => {
     await Entity.insertMany(mockEntity[0]);
     const entities = await request.get('/carryme');
     const entityID = entities.body[0]._id;
@@ -107,6 +97,5 @@ describe('Entity Endpoints', () => {
     expect(updatedEntity.statusCode).toBe(400);
     expect(updatedEntity.text).toBe('Validation failed');
     expect(updatedEntity.body).toEqual({});
-    done();
   });
 });
