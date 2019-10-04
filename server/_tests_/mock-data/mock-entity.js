d7ad691df4a1eb8eec823f5da30527f1ef789a23
Chris Rudnicky <chrisrudnicky@gmail.com>
2019-10-03 20:56:27 -0700

    add:
    new properties to the entity model and mock-entity data

--- server/_tests_/mock-data/mock-entity.js
+++ server/_tests_/mock-data/mock-entity.js
@@ -1,7 +1,14 @@
 const correctEntry = {
-  email: 'andersoncorp@gmail.com',
-  password: 'vallygurl13',
-  entityName: 'Anderson Valley Group',
+  // entityName: 'Anderson Valley Group',
+  // email: 'andersoncorp@gmail.com',
+  // password: 'vallygurl13',
+  // accountOwner_FirstName: 'Fred',
+  // accountOwner_LastName: 'Ackerson',
+  accountOwner_PhoneType: 'Cell',
+  accountOwner_PhoneNumber: '5718546323',
+  entityPhoneNumber: '7034846574',
+  createdAt: 'Thu Oct 03 2019 20:43:12 GMT-0700 (Pacific Daylight Time) {}',
+
 };
 const incorrectEntry = {
   password: 'vallygurl13',
--- server/src/models/Entity.model.js
+++ server/src/models/Entity.model.js
@@ -14,6 +14,50 @@ const EntitySchema = new Schema({
   password: {
     type: String,
     required: true,
+    unique: true,
+  },
+  accountOwner_FirstName: {
+    type: String,
+    required: true,
+  },
+  accountOwner_LastName: {
+    type: String,
+    required: true,
+  },
+  accountOwner_PhoneType: {
+   type: String,
+   required: true, 
+  },
+  accountOwner_PhoneNumber: {
+    type: String,
+    required: true,
+  },
+  entityPhoneNumber: {
+    type: Number,
+    required: true,
+  },
+  created: {
+    type: Date,
+    required: true,
+  },
+  street: {
+    type: String,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  state: {
+    type: String,
+    required: true,
+  },
+  zip: {
+    type: Number,
+    required: true,
+  },
+  contracts: {
+    type: Schema.Types.ObjectId,
   },
 });
 
