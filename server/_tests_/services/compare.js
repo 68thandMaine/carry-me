2cc506c87be244c5446a0ba892cf933e26e64da4
Chris Rudnicky <chrisrudnicky@gmail.com>
2019-10-14 21:48:52 -0700

    remove console logs from helper function in tests

--- server/_tests_/services/compare.js
+++ server/_tests_/services/compare.js
@@ -1,8 +1,6 @@
 exports.compareObjects = (expected, received) => {
   const expectedLength = Object.getOwnPropertyNames(expected).length;
   const receivedLength = Object.getOwnPropertyNames(received).length;
-  console.log(expectedLength)
-  console.log(receivedLength)
   /** Test to see if received matches a subset of expected properties */
   expect(received).toMatchObject(expected);
   // Test to see if the length of the original object has increased by 2.
