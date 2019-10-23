3fd14bf7c44812443cb873f80e600c3c02bb21b5
Michael Necula <N3CULA@N3CULA-MacBook-Pro-2.local>
2019-10-22 20:12:27 -0700

    Imported Landing Page css file

---
+++ client/src/components/initialAnnimation/svgViewer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './svgViewer.css';
+
+function SvgViewer() {
+    return (
+        <div className="svgViewerWrapper">CarryME</div>
+    )
+}
+
+export default SvgViewer;
