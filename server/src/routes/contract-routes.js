d1882622db7a9bf46af617bda545caa93a0f855f
Chris Rudnicky <chrisrudnicky@gmail.com>
2019-10-26 17:28:40 -0700

    add admin router file

--- server/src/routes/contract-routes.js
+++ server/src/routes/contract-routes.js
@@ -1,16 +1,7 @@
-// const Router = require('express').Router();
-// const contractController = require('../controllers/contractController.js');
-
-// Router.post('/:entityId/contract', contractController.create);
-// Router.get('/:entityId/contract', contractController.showEntityContracts);
-// Router.get('/:entityId/:contractId', contractController.showOneContract);
-// Router.put('/:entityId/:contractId', contractController.update);
-// Router.delete('/:entityId/:contractId', contractController.delete);
-
 module.exports.register = (server, serviceLocator) => {
   server.post('/entity/:entityId/contract', (req, res, next) => serviceLocator.get('contractController').create(req, res, next));
   server.get('entity/:entityId/contract', (req, res, next) => serviceLocator.get('contractController').index_EntityContracts(req, res, next));
   server.get('entity/:entityId/contract/:contractId', (req, res, next) => serviceLocator.get('contractController').show(req, res, next));
-  server.put('entity/:entityId/contract/:contractId', (req, res, next) => serviceLocator.get('contractController').update(req,res, next));
+  server.put('entity/:entityId/contract/:contractId', (req, res, next) => serviceLocator.get('contractController').update(req, res, next));
   server.delete('entity/:entityId/contract/:contractId', (req, res, next) => serviceLocator.get('contractController').delete(req, res, next));
 };
